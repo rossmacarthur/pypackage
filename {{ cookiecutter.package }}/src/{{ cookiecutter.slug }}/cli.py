912ef64d290bb3fb40b08af0af671fc5b30bcc88
Ross MacArthur <ross@macarthur.io>
2019-01-05 23:08:14 +0200

    Improve base CLI

--- {{ cookiecutter.package }}/src/{{ cookiecutter.slug }}/cli.py
+++ {{ cookiecutter.package }}/src/{{ cookiecutter.slug }}/cli.py
@@ -5,13 +5,15 @@ Console script for {{ cookiecutter.name }}.
 import click
 
 
-CONTEXT_SETTINGS = {'help_option_names': ['-h', '--help']}
-VERSION_PROG_NAME = '{{ cookiecutter.package }}'
-VERSION_MESSAGE = '%(prog)s %(version)s'
-
-
-@click.group(context_settings=CONTEXT_SETTINGS)
-@click.version_option(None, '-v', '--version', prog_name=VERSION_PROG_NAME, message=VERSION_MESSAGE)
+@click.group(
+    context_settings={'help_option_names': ['-h', '--help']}
+)
+@click.version_option(
+    None,
+    '-v', '--version',
+    prog_name='{{ cookiecutter.package }}',
+    message='%(prog)s %(version)s'
+)
 def cli():
     """
     {{ cookiecutter.description }}
