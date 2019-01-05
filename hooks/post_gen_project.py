b38fe1c09b3c9e8c859906f4b31edf41974dc28f
Ross MacArthur <ross@macarthur.io>
2019-01-05 23:33:55 +0200

    fix post gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,8 +3,14 @@
 import os
 
 
+DIRECTORY = os.path.realpath(os.path.curdir)
+
+
+def remove_file(*path):
+    os.remove(os.path.join(DIRECTORY, *path))
+
+
 if __name__ == '__main__':
     if '{{ cookiecutter.command_line_interface }}' == 'no':
-        directory = os.path.realpath(os.path.curdir)
-        path = ('src', '{{ cookiecutter.slug }}', 'cli.py')
-        os.remove(os.path.join(directory, *path))
+        remove_file('src', '{{ cookiecutter.slug }}', 'cli.py')
+        remove_file('tests', 'test_cli.py')
