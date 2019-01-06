f432708ee9d38311875751731edd57a3eae290b0
Ross MacArthur <ross@macarthur.io>
2019-01-06 08:00:24 +0200

    Improve CLI (#2)
    - Improve CLI code.
    - Add test for version option.
    - Update post gen project hook.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,8 +3,18 @@
 import os
 
 
-if __name__ == '__main__':
+DIRECTORY = os.path.realpath(os.path.curdir)
+
+
+def remove_file(*path):
+    os.remove(os.path.join(DIRECTORY, *path))
+
+
+def main():
     if '{{ cookiecutter.command_line_interface }}' == 'no':
-        directory = os.path.realpath(os.path.curdir)
-        path = ('src', '{{ cookiecutter.slug }}', 'cli.py')
-        os.remove(os.path.join(directory, *path))
+        remove_file('src', '{{ cookiecutter.slug }}', 'cli.py')
+        remove_file('tests', 'test_cli.py')
+
+
+if __name__ == '__main__':
+    main()
---
+++ {{ cookiecutter.package }}/tests/test_cli.py
@@ -0,0 +1,11 @@
+from click.testing import CliRunner
+
+from {{ cookiecutter.slug }} import __version__
+from {{ cookiecutter.slug }}.cli import cli
+
+
+def test_cli_version():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--version'])
+    assert result.exit_code == 0
+    assert result.output == '{{ cookiecutter.package }} {version}'.format(version=__version__)
