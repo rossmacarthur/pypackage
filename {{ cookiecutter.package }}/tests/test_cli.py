7841d83e3f86906fbc10af05e4fc33ba8940766a
Ross MacArthur <ross@macarthur.io>
2019-01-06 08:11:15 +0200

    Clean up setup.py, fix CLI test (#3)

--- {{ cookiecutter.package }}/tests/test_cli.py
+++ {{ cookiecutter.package }}/tests/test_cli.py
@@ -8,4 +8,4 @@ def test_cli_version():
     runner = CliRunner()
     result = runner.invoke(cli, ['--version'])
     assert result.exit_code == 0
-    assert result.output == '{{ cookiecutter.package }} {version}'.format(version=__version__)
+    assert result.output == '{{ cookiecutter.package }} {version}\n'.format(version=__version__)
