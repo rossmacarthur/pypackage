a12b4a5b1139bfaa4db6dd83b7ffdd8bdec451a2
Ross MacArthur <ross@macarthur.io>
2019-01-06 17:10:31 +0200

    Minor stylistic changes

--- {{ cookiecutter.package }}/src/{{ cookiecutter.slug }}/__init__.py
+++ {{ cookiecutter.package }}/src/{{ cookiecutter.slug }}/__init__.py
@@ -6,8 +6,8 @@
 
 __title__ = '{{ cookiecutter.package }}'
 __version__ = '{{ cookiecutter.version }}'
+__url__ = 'https://github.com/{{ cookiecutter.author_github_username }}/{{ cookiecutter.package }}'
 __author__ = '{{ cookiecutter.author_name }}'
 __author_email__ = '{{ cookiecutter.author_email }}'
-__url__ = 'https://github.com/{{ cookiecutter.author_github_username }}/{{ cookiecutter.package }}'
 __license__ = 'MIT'
 __description__ = '{{ cookiecutter.description }}'
