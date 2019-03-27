fa1efe01265f9bd1baa134039b6136bb0a1bf2be
Ross MacArthur <ross@macarthur.io>
2019-03-27 14:13:02 +0200

    Make more configurable (#9)
    - README markup is now an option.
    - Whether to include the Makefile, RELEASES file, and CONTRIBUTING file
    is now an option.
    - Split out .PHONY in Makefile.
    - Remove MANIFEST.in

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -16,16 +16,32 @@ def remove_file(*path):
 
 
 def main():
-    pass
-    {%- if cookiecutter.sphinx_documentation == 'no' %}
+  {% if cookiecutter.readme_markup_language != 'reStructuredText' %}
+    remove_file('README.rst')
+  {% endif %}
+  {% if cookiecutter.readme_markup_language != 'Markdown' %}
+    remove_file('README.md')
+  {% endif %}
+  {% if cookiecutter.license_file == 'no' %}
+    remove_file('LICENSE')
+  {% endif %}
+  {% if cookiecutter.sphinx_documentation == 'no' %}
     remove_directory('docs')
-    {%- endif %}
-    {%- if cookiecutter.command_line_interface == 'no' %}
+  {% endif %}
+  {% if cookiecutter.command_line_interface == 'no' %}
     remove_file('src', '{{ cookiecutter.slug }}', 'cli.py')
     remove_file('tests', 'test_cli.py')
-    {%- endif %}
+  {% endif %}
+  {% if cookiecutter.makefile == 'no' %}
+    remove_file('Makefile')
+  {% endif %}
+  {% if cookiecutter.contributing_file == 'no' %}
+    remove_file('CONTRIBUTING.rst')
+  {% endif %}
+  {% if cookiecutter.releases_file == 'no' %}
+    remove_file('RELEASES.rst')
+  {% endif %}
 
 
 if __name__ == '__main__':
     main()
-
